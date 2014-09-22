0238e918a11693751ea48b26c2dd9e67e52b053e
Baptiste Fallas <baptiste@rapidocteur.fr>
2014-09-22 21:17:22 +0200

    solve crash in multiple calls to the same endpoint

--- Back/backend-app.js
+++ Back/backend-app.js
@@ -1,36 +1,35 @@
 var express = require('express');
 var pg = require('pg');
-var conString = "postgres://baptiste@localhost/rapidocteur";
+var connectionString = "postgres://baptiste@localhost/rapidocteur";
 
 var app = express();
 
-var client = new pg.Client(conString);
-
 function processReqResSql(req, res, sql){
-	client.connect(function(err) {
-		  if(err) {
-		    return console.error('could not connect to postgres', err);
-		  }
-		  client.query(sql, function(err, result) {
-		    if(err) {
-		      return console.error('error running query', err);
-		    }
-		    var sr = new ServiceResult ('success');
-		            sr.output = result.rows;
-		            //for (i = 0; i < result.rows.length; i++) {
-		            //    sr.output[i] = JSON.parse(result.rows[i].data);
-		            //}
-		            console.log(new Date() + '  ' + sql);
-		            res.send(sr);
-		    client.end();
-		  });
+	pg.connect(connectionString, function(err, client, done) {
+		if(err) {
+			return console.error('could not connect to postgres', err);
+		}
+		client.query(sql, function(err, result) {
+			if(err) {
+				return console.error('error running query', err);
+			}
+			var sr = new ServiceResult ('success');
+			sr.output = result.rows;
+            //for (i = 0; i < result.rows.length; i++) {
+            //    sr.output[i] = JSON.parse(result.rows[i].data);
+            //}
+            console.log(new Date() + '  ' + sql);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(sr);
+            done();
+        });
 	});
 }
 
 function ServiceResult (status){
-    this.ts = new Date();
-    this.status = status;
-    this.output = new Array();
+	this.ts = new Date();
+	this.status = status;
+	this.output = new Array();
 }
 
 app.get('/', function(req, res) {
@@ -39,19 +38,19 @@ app.get('/', function(req, res) {
 });
 
 app.get('/medecins', function(req, res) {
-    var sql = 'select * '
-+ 'from \"Praticien\", \"Possede\", \"Specialite\"'
-+ 'where \"Praticien\".\"IdPraticien\" = \"Possede\".\"IdPraticien_Praticien\"'
-+ 'and \"Possede\".\"idSpecialite_Specialite\" = \"Specialite\".\"idSpecialite\"';
-    processReqResSql(req, res, sql);
-  }
+	var sql = 'select * '
+	+ 'from \"Praticien\", \"Possede\", \"Specialite\"'
+	+ 'where \"Praticien\".\"IdPraticien\" = \"Possede\".\"IdPraticien_Praticien\"'
+	+ 'and \"Possede\".\"idSpecialite_Specialite\" = \"Specialite\".\"idSpecialite\"';
+	processReqResSql(req, res, sql);
+}
 );
 
 app.get('/specialites', function(req, res) {
-    var sql = 'select * '
-+ 'from \"Specialite\"';
-    processReqResSql(req, res, sql);
-  }
+	var sql = 'select * '
+	+ 'from \"Specialite\"';
+	processReqResSql(req, res, sql);
+}
 );
 
 app.listen(8080);
