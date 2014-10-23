ab96056d94002f8b629edd178812288be2cf051e
Gwendoline Meyer <gwendoline@rapidocteur.fr>
2014-10-23 14:16:16 +0200

    update controller bertucchi

--- js/controllers.js
+++ js/controllers.js
@@ -163,18 +163,42 @@ appControllers.controller('PresentationMelanieTachier', ['$scope', 'GetService',
 
 appControllers.controller('PresentationYouriBertucchi', ['$scope', 'GetService', 'AppointmentManager',
 	function($scope, GetService, AppointmentManager){
-		var idCabinet = 5; 
+		var idCabinet = 5; //cabinet de mélanie : id 4 //à modifier
 		$scope.quantityWeek=7;
 		$scope.quantityApp=5;
 
-		GetService.getAppointementsByDoctorsInOffice(idCabinet).success(function(data) {
+		GetService.getDoctorsByOffice(idCabinet).success(function(data) {
+			console.log('getDoctorsByOffice : ', data);
 			$scope.doctors = data.output;
-			console.log($scope.doctors);
+			// initialise select elements
+			angular.forEach($scope.doctors, function(doctor){
+				doctor["selectedAct"] = doctor.acts[0];
 
+				console.log("doctor.selectedAct", doctor.selectedAct);
+
+				GetService.getAvailableAppointements(idCabinet, doctor.idPraticien, doctor.selectedAct.duree).success(function(data) {
+					console.log('getAvailableAppointements : ', data);
+					doctor["availabilities"] = data.output;
+				}).error(function(data, status) {
+					console.log(status);
+					console.log(data);
+				});
+
+			});
 		}).error(function(data, status) {
-			console.log('response getDoctorByOffice : ' + status);
-			console.log('response getDoctorByOffice : ' + data);
+			console.log(status);
+			console.log(data);
 		});	
+
+		$scope.updateAvailableAppointments = function(doctor){
+			GetService.getAvailableAppointements(idCabinet, doctor.idPraticien, doctor.selectedAct.duree).success(function(data) {
+				console.log('getAvailableAppointements : ', data);
+				doctor["availabilities"] = data.output;
+			}).error(function(data, status) {
+				console.log(status);
+				console.log(data);
+			});
+		}
 
 		$scope.submitRV = function(doctor, day, id, acte){
 
