646a240287d4171afbf5138cc63ca7a1da40007b
Nicolas Toublanc <n.toublanc@gmail.com>
2015-02-20 16:56:21 +0100

    Migration to node

---
+++ public/js/angular-app.js
@@ -0,0 +1,9 @@
+(function () {
+
+    var app = angular.module('app', ['ui.bootstrap']);
+
+    var appServices = angular.module('appServices', []);
+    var appControllers = angular.module('appControllers', []);
+    var appConfig = angular.module('appConfig', []);
+
+})();
