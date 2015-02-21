3a606fcfb15f21739bfae28826f04ade05ca0c73
Nicolas Toublanc <n.toublanc@gmail.com>
2015-02-21 15:13:21 +0100

    introduce js-data

---
+++ app/modules/praticien/praticien.js
@@ -0,0 +1,40 @@
+(function () {
+    "use strict";
+
+    var viewRenderer = require('../../view-renderer');
+
+    var express = require('express');
+    var router = express.Router();
+
+    var JSData = require('js-data');
+    var DSHttpAdapter = require('js-data-http');
+    var DSProvider = new JSData.DS();
+
+    module.exports = function (app) {
+        app.use('/praticien', router.get('/', render));
+    };
+
+    function render(req, res) {
+
+        DSProvider.registerAdapter('http', new DSHttpAdapter({
+            // url: 'http://0.0.0.0:88',
+            log: console.log
+        }), {
+            default: true
+        });
+        DSProvider.defaults.basePath = 'http://0.0.0.0:88/api';
+        DSProvider.defaults.idAttribute = '_id';
+
+        // simplest model definition
+        var Praticien = DSProvider.defineResource('praticiens');
+
+        DSProvider.findAll('praticiens').then(function (praticiens) {
+            console.log(praticiens);
+            viewRenderer.render('praticien', {
+                praticiens: praticiens
+            }, res);
+        });
+
+    }
+
+})();
