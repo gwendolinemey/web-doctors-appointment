7bf366df73edc02afcbfa22bc9ecd9fc936c5834
Nicolas Toublanc <n.toublanc@gmail.com>
2015-02-24 12:02:20 +0100

    custum marker

--- public/js/directives/doctor-map/doctor-map.directive.js
+++ public/js/directives/doctor-map/doctor-map.directive.js
@@ -14,6 +14,8 @@
             link: {
                 pre: function preLink($scope, $element, $attrs) {
 
+
+
                     $scope.$watch('praticien', function (praticien, old) {
 
                         if (praticien) {
@@ -24,20 +26,54 @@
 
                             console.log('Map center: %d, %d', latitude, longitude);
 
+                            var point = ol.proj.transform([longitude, latitude], /* WGS84 */ 'EPSG:4326', /* Google */ 'EPSG:3857');
+
+                            // http://openlayers.org/en/v3.2.1/examples/data/icon.png
+
+                            var point2 = new ol.geom.Point(point);
+
+
+                            var iconFeature = new ol.Feature({
+                                geometry: point2
+                            });
+
+
+                            var iconStyle = new ol.style.Style({
+                                image: new ol.style.Icon(({
+                                    anchor: [0.5, 46],
+                                    anchorXUnits: 'fraction',
+                                    anchorYUnits: 'pixels',
+                                    opacity: 0.75,
+                                    src: '/img/marker.png'
+                                }))
+                            });
+
+                            iconFeature.setStyle(iconStyle);
+
+                            var vectorSource = new ol.source.Vector({
+                                features: [iconFeature]
+                            });
+
+                            var vectorLayer = new ol.layer.Vector({
+                                source: vectorSource
+                            });
+
+
+
+                            var mainLayer = new ol.layer.Tile({
+                                source: new ol.source.MapQuest({
+                                    layer: 'osm'
+                                })
+                            });
+
                             var map = new ol.Map({
                                 target: 'map',
-                                layers: [
-      new ol.layer.Tile({
-                                        source: new ol.source.MapQuest({
-                                            layer: 'osm'
-                                        })
-                                    })
-    ],
+                                layers: [mainLayer, vectorLayer],
                             });
 
                             map.setView(new ol.View({
-                                center: ol.proj.transform([longitude, latitude], 'EPSG:4326', 'EPSG:3857'),
-                                zoom: 12
+                                center: point,
+                                zoom: 10
                             }));
 
                         }
